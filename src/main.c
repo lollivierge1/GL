f88c126ed71e0cfc6de822f485d8e278adfe4075
lollivierge1 <44799220+lollivierge1@users.noreply.github.com>
2018-11-15 20:13:57 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,5 +5,28 @@
 
 int main(void)
 {
-    return 0;
+   #include <ansi_c.h>
+#include <TP_lib.h>
+
+int main(void) 
+ {
+	 short resultat =0;
+	 int somme =0;
+	 while (resultat !=1)
+	 {
+		 resultat = LancerDe();
+		 
+		 printf("Resultat %d \n", resultat);
+		 
+		 if (resultat ==2 || resultat ==4 || resultat == 6) { somme = somme + resultat;}
+		 if (resultat ==3) { somme = somme *2;}
+		 if (resultat ==5) 
+		 	{ 
+			 somme = somme -2;
+			 	if (somme <0) {somme = 0;}
+		 	} 
+		printf("Somme =  %d \n", somme);	
+	 }
+	 
+	 return 0;
 }
