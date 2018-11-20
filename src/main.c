1f6416a897ba394d56601a2d2a7e2a4b12ee2aa9
luc ollivier <luc.ollivier@ge1.iut-cachan.u-psud.fr>
2018-11-20 09:50:35 +0100

    j'ai démareé exo56

--- src/main.c
+++ src/main.c
@@ -4,6 +4,26 @@
 #include "my_header.h"
 
 int main(void)
+
 {
+	unsigned char bin[10] =
+	{0,1,2,3,4,5,6,7,8,9};
+	
+	unsigned char seuil = 5;
+	unsigned char i=0;
+	{
+		for (i=0; i<10; i=i+1)
+		{
+						
+	if (bin[i]>seuil)
+		bin[i]=1;
+		
+	else bin[i]=0;
+		}			
+	
+	}
+						
+						
+						
     return 0;
 }
