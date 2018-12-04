45757bd55dc010b0203dc6ead2f129a7c95972b8
luc ollivier <luc.ollivier@ge1.iut-cachan.u-psud.fr>
2018-12-04 09:53:43 +0100

    exo commence

--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <ansi_c.h>
 #include "my_header.h"
 
 int main(void)
 {
+	char phrase[100]= "je m'appelle luc ollivier et j'ai 18 ans";
+	char chaine[100];
+	char prenom [100];
+	unsigned char nom;
+	unsigned char age;
+	
+	printf("%s", phrase);
+	scanf ("%s", chaine);
+	sprintf (phrase, "prenom, nom, age");
+	sscanf(phrase,"%*s %*s %s %s %*s %*s %u %lu", prenom, &nom, &age);
+	
     return 0;
 }
