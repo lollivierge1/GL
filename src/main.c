202561ad15f6eed7a6d35f50229769c5cd6dd0a2
lollivierge1 <44799220+lollivierge1@users.noreply.github.com>
2019-01-07 20:39:42 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,17 +2,17 @@
 //
 
 #include <userint.h>
-#include "my_header.h"
+//#include "my_header.h"
 #include <ansi_c.h>
 
-int compare(const char* chaine1, const char* chaine2);
+	
 
 int main(void)
 {
 	
 	char ligne1[80] = "Nom: pentagone_B";
 	char ligne2[80] = "Nb points: 5"; 
-	char ligne3[80] = "Coordonnees: 10 20 30 20 30 40 43 53 35 65";
+	char ligne3[80] = "Coordonnees: 10 20 30 20 30 40 43 53 -1 65";
 
 	char nom_fig[20]; 
 	int x[5] = {0,0,0,0,0};
@@ -21,7 +21,18 @@ int main(void)
 	
 	sscanf (ligne1, "%*s %s", nom_fig);
 	sscanf (ligne2, "%*s %*s %d", &nb_pts);   
-	sscanf (ligne3, "%*s %d %d %d %d %d %d %d %d %d %d", &x[0], &x[1], &x[2], &x[3], &x[4], &y[0], &y[1], &y[2], &y[3], &y[4]);
+	sscanf (ligne3, "%*s %d %d %d %d %d %d %d %d %d %d", &x[0], &y[0], &x[1], &y[1], &x[2], &y[2], &x[3], &y[3], &x[4], &y[4]);
+
+	
+	char messageErreur[80]; 
+	for (int i=0; i<5; i++)
+	{
+		if ((x[i] == -1  && y[i] != -1) || (x[i] != -1  && y[i] == -1 ))
+		{ sprintf (messageErreur, "Les coordonnees sont incorrectes");
+		  MessagePopup ("Attention" , messageErreur);
+		}
+	}	
+	
 	
 
 	char message[80];
@@ -30,4 +41,5 @@ int main(void)
 	
     return 0;
 }
+
 
